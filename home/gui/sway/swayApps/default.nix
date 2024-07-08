c75c8df1f2a74768e07f35e130806d6b81b64375
kubaVencel <105190657+KubaVencel@users.noreply.github.com>
2024-07-08 20:02:50 +0200

    fixing wlogout

--- home/gui/sway/swayApps/default.nix
+++ home/gui/sway/swayApps/default.nix
@@ -52,8 +52,8 @@
       button {
         color: rgba(17, 17, 27, 0.15);
         background-color: rgba(147, 153, 178, 0.45);
-        border-style: solid;
-        border: 3px solid;
+        #`border-style: solid;
+        #border: 3px solid;
         background-repeat: no-repeat;
         background-position: center;
         background-size: 25%;
