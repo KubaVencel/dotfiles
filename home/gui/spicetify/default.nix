9a05d7b547e7d90a3bd36ca032c7e81c8bd80056
kubaVencel <105190657+KubaVencel@users.noreply.github.com>
2025-03-29 20:19:27 +0100

    spicetify test

---
+++ home/gui/spicetify/default.nix
@@ -0,0 +1,15 @@
+{pkgs, config, inputs, ...}:
+{
+  imports = [
+    inputs.spicetify-nix.homeManagerModules.default
+  ];
+
+  programs.spicetify =
+    let
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in
+      {
+      enable = true;
+      theme = spicePkgs.themes.catppuccin;
+    };
+}
