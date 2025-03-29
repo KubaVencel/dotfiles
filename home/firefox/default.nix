5108bdb5558fc10829aef9d4683d45ddd4424e85
kubaVencel <105190657+KubaVencel@users.noreply.github.com>
2025-03-29 22:02:45 +0100

    back to firefox

---
+++ home/firefox/default.nix
@@ -0,0 +1,74 @@
+{ lib, inputs, pkgs, ... }:
+{
+  programs.firefox = {
+    enable = true;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+    };
+    profiles.echoes = {
+      search.engines = {
+        "kagi" = {
+          urls = [{ template = "https://kagi.com/search?q={searchTerms}"; }];
+          icon = "󰩃 ";
+          iconColor = "brown";
+          definedAliases = [ "@kagi" ];
+        };
+        "ecosia" = {
+          urls = [{ template = "https://www.ecosia.org/search?q={searchTerms}"; }];
+          icon = " ";
+          definedAliases = [ "@ecosia" ];
+        };
+
+        "Nix Packages" = {
+          urls = [{
+            template = "https://search.nixos.org/packages";
+            params = [
+              { name = "type"; value = "packages"; }
+              { name = "query"; value = "{searchTerms}"; }
+            ];
+          }];
+          icon = "󱄅 ";
+          #icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = [ "@np" ];
+        };
+      };
+      
+      search = {
+        force = true;
+        default = "kagi";
+      };
+
+      bookmarks = {
+        force = true;
+        settings = [
+          {
+            name = "wikipedia";
+            tags = [ "wiki" ];
+            keyword = "wiki";
+            url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+        }
+      ];
+    };
+
+
+      extraConfig = "${builtins.readFile ./user.js}";
+
+      userChrome = "${builtins.readFile  ./userChrome.css}";	  
+      
+      userContent = "${builtins.readFile ./userContent.css}";
+
+      extensions.packages = with inputs.firefox-addons.packages."x86_64-linux"; [
+        bitwarden
+	decentraleyes
+	darkreader
+        ublock-origin
+        privacy-badger
+        return-youtube-dislikes
+        #sponsorblock
+        tridactyl
+      ];
+      # https://www.deviceinfo.me/
+    };
+  };
+}
