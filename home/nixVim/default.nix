0751c32be65a6ffd618a11888866c1645a3517d8
kubaVencel <105190657+KubaVencel@users.noreply.github.com>
2025-06-29 15:15:25 +0200

    nixvim

--- home/nixVim/default.nix
+++ home/nixVim/default.nix
@@ -1,5 +1,6 @@
 { pkgs, inputs, ... }: {
   imports = [
+    ./plugins/keymaps.nix
     # Plugins
     ./plugins/gitsigns.nix
     ./plugins/which-key.nix
@@ -117,13 +118,34 @@
       
 #     scrolloff = 3;
 #     textwidth = 100;
-#     cursorline = true;
+      #cursorline = true;
 #     cursorcolumn = true;
-#     termguicolors = true;
+      termguicolors = true;
 #     background = "dark";
 #     showmode = false;
-  
-      shiftwidth = 2; # Tab width should be 2
+      winblend = 0;
+      wildoptions = "pum";
+      pumblend = 5;
+
+      tabstop = 4;
+      softtabstop = 4;
+      shiftwidth = 4;
+      autoindent = true;
+      expandtab = true;
+      smartindent = true;
+      wrap = false;
+      swapfile = false;
+
+      hidden = true;
+      whichwrap = "b,s,<,>,[,],h,l";
+      pumheight = 8;
+      fileencoding = "utf-8";
+      cmdheight = 1;
+      conceallevel = 0;
+      showtabline = 2;
+      backup = false;
+      writebackup = false;
+      sidescrolloff = 3;
 
       # Enable mouse mode, can be useful for resizing splits for example!
       mouse = "a";
@@ -145,11 +167,13 @@
       signcolumn = "yes";
 
       # Decrease update time
-      updatetime = 250;
+      updatetime = 50;
+
+      colorcolumn = "80";
 
       # Decrease mapped sequence wait time
       # Displays which-key popup sooner
-      timeoutlen = 300;
+      timeoutlen = 100;
 
       # Configure how new splits should be opened
       splitright = true;
@@ -174,91 +198,6 @@
       # See `:help hlsearch`
       hlsearch = true;
     };
-
-    # [[ Basic Keymaps ]]
-    #  See `:help vim.keymap.set()`
-    # https://nix-community.github.io/nixvim/keymaps/index.html
-    keymaps = [
-      # Clear highlights on search when pressing <Esc> in normal mode
-      {
-        mode = "n";
-        key = "<Esc>";
-        action = "<cmd>nohlsearch<CR>";
-      }
-      # Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
-      # for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
-      # is not what someone will guess without a bit more experience.
-      #
-      # NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
-      # or just use <C-\><C-n> to exit terminal mode
-      {
-        mode = "t";
-        key = "<Esc><Esc>";
-        action = "<C-\\><C-n>";
-        options = {
-          desc = "Exit terminal mode";
-        };
-      }
-
-      # TIP: Disable arrow keys in normal mode
-      {
-        mode = "n";
-        key = "<left>";
-        action = "<cmd>echo 'Use h to move!!'<CR>";
-      }
-      {
-        mode = "n";
-        key = "<right>";
-        action = "<cmd>echo 'Use l to move!!'<CR>";
-      }
-      {
-        mode = "n";
-        key = "<up>";
-        action = "<cmd>echo 'Use k to move!!'<CR>";
-      }
-      {
-        mode = "n";
-        key = "<down>";
-        action = "<cmd>echo 'Use j to move!!'<CR>";
-      }
-
-      # Keybinds to make split navigation easier.
-      #  Use CTRL+<hjkl> to switch between windows
-      #
-      #  See `:help wincmd` for a list of all window commands
-      {
-        mode = "n";
-        key = "<C-h>";
-        action = "<C-w><C-h>";
-        options = {
-          desc = "Move focus to the left window";
-        };
-      }
-      {
-        mode = "n";
-        key = "<C-l>";
-        action = "<C-w><C-l>";
-        options = {
-          desc = "Move focus to the right window";
-        };
-      }
-      {
-        mode = "n";
-        key = "<C-j>";
-        action = "<C-w><C-j>";
-        options = {
-          desc = "Move focus to the lower window";
-        };
-      }
-      {
-        mode = "n";
-        key = "<C-k>";
-        action = "<C-w><C-k>";
-        options = {
-          desc = "Move focus to the upper window";
-        };
-      }
-    ];
 
     # https://nix-community.github.io/nixvim/NeovimOptions/autoGroups/index.html
     autoGroups = {
